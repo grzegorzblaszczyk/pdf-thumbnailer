35122b06efad944606b058ea7a6b4846b6376ba0
Grzegorz Błaszczyk <grzegorz.blaszczyk@sparq-systems.com>
2011-08-13 23:12:32 +0200

    ADD: sample config file, TYPO: LAOD -> LOAD

--- create_pdf_thumbnail.sh
+++ create_pdf_thumbnail.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+# Create PDF Thumbnail v0.2
+
+# Changelog:
+#
+# 0.2 - added upload feature via SCP and use of ~/.pdf_thumbnailer.rc file
+# 0.1 - initial version
+
+# About:
 # Fast'n'dirty script by Grzegorz Błaszczyk <grzegorz@blaszczyk-consulting.com>
 
+AWK=`which awk`
 CAT=`which cat`
 CUT=`which cut`
 GREP=`which grep`
+SCP=`which scp`
 WC=`which wc`
-CONVERT=`which convertee`
+CONVERT=`which convert`
 HEIGHT=259
 
+CONFIG_FILE="$HOME/.pdf_thumbnailer.rc"
+
 ### DO NOT EDIT BELOW THIS LINE ###
+
+function read_config_file_property {
+  VALUE=`$CAT $CONFIG_FILE | $GREP $1 | $CUT -f 2 -d '='`
+  echo $VALUE
+}
 
 function print_usage {
   echo "Usage: $0 [PDF filename]"
@@ -19,6 +36,10 @@ function print_usage {
 if [ -z $1 ]; then
   print_usage
 fi
+
+UPLOAD_USERNAME=`read_config_file_property "scp_upload_username"`
+UPLOAD_HOST=`read_config_file_property "scp_upload_hostname"`
+UPLOAD_DIR=`read_config_file_property "scp_upload_directory"`
 
 if [ -z $CONVERT ]; then 
   echo ""
@@ -53,4 +74,10 @@ echo -n "Converting..."
 #echo $CONVERT : $HEIGHT : $PDF_FILENAME\[0\] : $JPG_FILENAME
 $CONVERT -scale x$HEIGHT $PDF_FILENAME\[0\] $JPG_FILENAME
 echo "[done]"
+
+read -p "Do you want to upload the book along with the cover to the server [Y/n]? "
+if [ "$REPLY" != "n" ]; then 
+  echo "Uploading files ${PDF_FILENAME%\.pdf}.* to $UPLOAD_USERNAME@$UPLOAD_HOST:$UPLOAD_DIR"
+  $SCP -v ${PDF_FILENAME%\.pdf}.* $UPLOAD_USERNAME"@"$UPLAOD_HOST":"$UPLOAD_DIR
+fi
 
