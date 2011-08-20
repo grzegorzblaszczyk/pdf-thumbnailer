40941019923e89abf025482240a3aa80b056a83a
Grzegorz Błaszczyk <grzegorz.blaszczyk@gmail.com>
2011-08-20 12:21:01 +0200

    ADD: creating empty config file if it not exists, FIX: UPLAOD_HOST -> UPLOAD_HOST

--- create_pdf_thumbnail.sh
+++ create_pdf_thumbnail.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Create PDF Thumbnail v0.2
+# Create PDF Thumbnail v0.2.1
 # Changelog:
 #
+# 0.2.1 - fix when you do not have an .pdf_thumbnailer.rc file we create one for you with default settings
 # 0.2 - added upload feature via SCP and use of ~/.pdf_thumbnailer.rc file
 # 0.1 - initial version
 
 # About:
 # Fast'n'dirty script by Grzegorz Błaszczyk <grzegorz@blaszczyk-consulting.com>
+
+VERSION="0.2.1"
 
 AWK=`which awk`
 CAT=`which cat`
@@ -21,6 +24,14 @@ HEIGHT=259
 CONFIG_FILE="$HOME/.pdf_thumbnailer.rc"
 
 ### DO NOT EDIT BELOW THIS LINE ###
+
+if [ ! -f $CONFIG_FILE ]; then 
+  echo "Configuration file not found - creating $CONFIG_FILE..."
+  echo "#PDF Thumbnail v$VERSION" > $CONFIG_FILE
+  echo "scp_upload_username=$USER" >> $CONFIG_FILE
+  echo "scp_upload_hostname=somehost.com" >> $CONFIG_FILE
+  echo "scp_upload_directory=/home/$USER" >> $CONFIG_FILE
+fi
 
 function read_config_file_property {
   VALUE=`$CAT $CONFIG_FILE | $GREP $1 | $CUT -f 2 -d '='`
@@ -77,6 +88,6 @@ echo "[done]"
 read -p "Do you want to upload the book along with the cover to the server [Y/n]? "
 if [ "$REPLY" != "n" ]; then 
   echo "Uploading files ${PDF_FILENAME%\.pdf}.* to $UPLOAD_USERNAME@$UPLOAD_HOST:$UPLOAD_DIR"
-  $SCP -v ${PDF_FILENAME%\.pdf}.* $UPLOAD_USERNAME"@"$UPLAOD_HOST":"$UPLOAD_DIR
+  $SCP -v ${PDF_FILENAME%\.pdf}.* $UPLOAD_USERNAME"@"$UPLOAD_HOST":"$UPLOAD_DIR
 fi
 
