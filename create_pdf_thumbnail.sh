3fd58c9e25e63b8b297ae582043a379ae4384a44
Grzegorz Błaszczyk <grzegorz.blaszczyk@gmail.com>
2013-01-13 15:13:20 +0100

    It works also on Mac OS X

--- create_pdf_thumbnail.sh
+++ create_pdf_thumbnail.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Create PDF Thumbnail v0.2.1
+# Create PDF Thumbnail v0.3.
 # Changelog:
 #
+# 0.3 - working also on Mac OS X
 # 0.2.1 - fix when you do not have an .pdf_thumbnailer.rc file we create one for you with default settings
 # 0.2 - added upload feature via SCP and use of ~/.pdf_thumbnailer.rc file
 # 0.1 - initial version
@@ -10,13 +11,14 @@
 # About:
 # Fast'n'dirty script by Grzegorz Błaszczyk <grzegorz@blaszczyk-consulting.com>
 
-VERSION="0.2.1"
+VERSION="0.3"
 
 AWK=`which awk`
 CAT=`which cat`
 CUT=`which cut`
 GREP=`which grep`
 SCP=`which scp`
+UNAME=`which uname`
 WC=`which wc`
 CONVERT=`which convert`
 HEIGHT=259
@@ -43,6 +45,19 @@ function print_usage {
   exit 1;
 }
 
+function get_os_version {
+  LSB_RELEASE_FILE="/etc/lsb-release"
+  if [ -f $LSB_RELEASE_FILE ]; then
+    DISTRO_ID=`$CAT /etc/lsb-release | $GREP "ID" | $CUT -f 2 -d "="`;
+    echo $DISTRO_ID;
+  else
+    UNAME_STRING=`$UNAME`
+    if [ "x$UNAME_STRING" == "xDarwin" ]; then
+      echo "MacOSX";
+    fi
+  fi
+}
+
 if [ -z $1 ]; then
   print_usage
 fi
@@ -59,19 +74,22 @@ if [ -z $CONVERT ]; then
   echo ""
   echo "Please install ImageMagick package."
   echo ""
+  echo "You can install it by running the following command:"
 
-  DISTRO_ID=`$CAT /etc/lsb-release | $GREP "ID" | $CUT -f 2 -d "="`
- 
-  case $DISTRO_ID in
+  OS_VERSION=`get_os_version`
+
+  case $OS_VERSION in
     "Ubuntu" )
-      echo "You can install it by running the following command:"
       echo "sudo apt-get install imagemagick" ;;
     "Fedora" )
-      echo "You can install it by running the following command:"
       echo "sudo yum install imagemagick" ;;
     "CentOS" )
-      echo "You can install it by running the following command:"
       echo "sudo yum install imagemagick" ;;
+    "MacOSX" )
+      echo "brew install ghostscript";
+      echo "brew install imagemagick" ;;
+    "Unknown" )
+      echo "[Your operating system is not known to this script - please install ImageMagick on your own.]"
   esac
   echo ""
   exit 1;
@@ -81,7 +99,7 @@ PDF_FILENAME=$1
 JPG_FILENAME="${PDF_FILENAME%\.pdf}.jpg"
 
 echo -n "Converting..."
-#echo $CONVERT : $HEIGHT : $PDF_FILENAME\[0\] : $JPG_FILENAME
+echo $CONVERT : $HEIGHT : $PDF_FILENAME[0] : $JPG_FILENAME
 $CONVERT -scale x$HEIGHT $PDF_FILENAME\[0\] $JPG_FILENAME
 echo "[done]"
 
@@ -90,4 +108,3 @@ if [ "$REPLY" != "n" ]; then
   echo "Uploading files ${PDF_FILENAME%\.pdf}.* to $UPLOAD_USERNAME@$UPLOAD_HOST:$UPLOAD_DIR"
   $SCP -v ${PDF_FILENAME%\.pdf}.* $UPLOAD_USERNAME"@"$UPLOAD_HOST":"$UPLOAD_DIR
 fi
-
